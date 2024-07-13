17748a67f1c5d67cf7de40b1eb1c883e0d8c3eed
Ilham Ghazali <m.ilhamghazali@gmail.com>
2024-07-13 16:47:04 +0700

    add: navigation groups and items for filament admin panel

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -2,10 +2,20 @@
 
 namespace App\Providers\Filament;
 
+use App\Filament\Resources\CourierResource;
+use App\Filament\Resources\CustomerResource;
+use App\Filament\Resources\DeliveryResource;
+use App\Filament\Resources\FlowerArrangementResource;
+use App\Filament\Resources\OrderDetailResource;
+use App\Filament\Resources\OrderResource;
+use App\Filament\Resources\PaymentResource;
+use App\Filament\Resources\UserResource;
 use Filament\Http\Middleware\Authenticate;
 use Filament\Http\Middleware\DisableBladeIconComponents;
 use Filament\Http\Middleware\DispatchServingFilamentEvent;
+use Filament\Navigation\NavigationGroup;
 use Filament\Pages;
+use Filament\Pages\Dashboard;
 use Filament\Panel;
 use Filament\PanelProvider;
 use Filament\Support\Colors\Color;
@@ -54,5 +64,40 @@ class AdminPanelProvider extends PanelProvider
             ->authMiddleware([
                 Authenticate::class,
             ]);
+            // ->navigationGroups($this->getNavigationGroups()); // Menggunakan navigationGroups
     }
+
+    // public function getNavigationGroups(): array
+    // {
+    //     return [
+    //         // Group: Dashboard
+    //         NavigationGroup::make('Dashboard')
+    //             ->items([
+    //                 Pages\Dashboard::class, // Menambahkan Dashboard ke navigasi
+    //             ]),
+
+    //         // Group 1: Customers and Users
+    //         NavigationGroup::make('Customers and Users')
+    //             ->items([
+    //                 UserResource::getNavigation(),
+    //                 CustomerResource::getNavigation(),
+    //                 CourierResource::getNavigation(),
+    //             ]),
+
+    //         // Group 2: Flowers
+    //         NavigationGroup::make('Flowers')
+    //             ->items([
+    //                 FlowerArrangementResource::getNavigation(),
+    //             ]),
+
+    //         // Group 3: Orders and Deliveries
+    //         NavigationGroup::make('Orders and Deliveries')
+    //             ->items([
+    //                 OrderResource::getNavigation(),
+    //                 OrderDetailResource::getNavigation(),
+    //                 PaymentResource::getNavigation(),
+    //                 DeliveryResource::getNavigation(),
+    //             ]),
+    //     ];
+    //}
 }
