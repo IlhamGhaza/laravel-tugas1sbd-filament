0bb03f8d555d092fa61f5962a603c030188daaf2
Ilham Ghazali <m.ilhamghazali@gmail.com>
2024-07-18 05:56:16 +0700

    feat(api): create CourierController to handle API requests for courier details

---
+++ app/Http/Controllers/Api/CourierController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Models\Courier;
+use Orion\Http\Controllers\Controller;
+
+class CourierController extends Controller
+{
+    protected $model = Courier::class;
+}
