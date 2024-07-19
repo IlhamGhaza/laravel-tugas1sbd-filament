81c0b25cb2bba4c06775eec60ce162c6812d3b02
Ilham Ghazali <m.ilhamghazali@gmail.com>
2024-07-19 08:57:19 +0700

    feat(api): refactor API routes for auth, flower-arrangements, customers, and couriers with Orion resource management

--- routes/api.php
+++ routes/api.php
@@ -1,25 +1,30 @@
 <?php
 
 use App\Http\Controllers\Api\AuthController as ApiAuthController;
+use App\Http\Controllers\Api\CourierController;
+use App\Http\Controllers\Api\CustomerController;
 use App\Http\Controllers\Api\FlowerArrangementController;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\FlowerController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Orion\Facades\Orion;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-//auth
-Route::post('/register', [ApiAuthController::class, 'register']);
-Route::post('/login', [ApiAuthController::class, 'login']);
-Route::post('/logout', [ApiAuthController::class, 'logout'])->middleware('auth:sanctum');
+Route::group(['as' => 'api.'], function() {
+    //auth
+    Route::post('/register', [ApiAuthController::class, 'register']);
+    Route::post('/login', [ApiAuthController::class, 'login']);
+    Route::post('/logout', [ApiAuthController::class, 'logout'])->middleware('auth:sanctum');
 
-//flower api
-Route::resource('flower-arrangements', FlowerArrangementController::class)
-// ->except(['create', 'edit'])
-->middleware('auth:sanctum');
+    //flower
+    Orion::resource('flower-arrangements', FlowerArrangementController::class)->withSoftDeletes()->middleware('auth:sanctum');
+    //customer
+    Orion::resource('customers', CustomerController::class)->withSoftDeletes()->middleware('auth:sanctum');
+    //courier
+    Orion::resource('couriers', CourierController::class)->withSoftDeletes()->middleware('auth:sanctum');
 
-
-
+});
