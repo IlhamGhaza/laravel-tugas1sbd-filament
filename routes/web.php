014d49c1b4617277dfee584d7176b468e60e7760
Ilham Ghazali <m.ilhamghazali@gmail.com>
2024-07-21 13:43:08 +0700

    feat(routes): add POST route for /orders to create new order records

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\OrderController;
+
+Route::post('/orders', [OrderController::class, 'store']);
 
 Route::get('/', function () {
     return view('welcome');
