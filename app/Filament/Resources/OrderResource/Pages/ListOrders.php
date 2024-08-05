3f585433c46d9628f074ab1d66c14df1f3f1fb2d
Ilham Ghazali <m.ilhamghazali@gmail.com>
2024-08-05 10:52:34 +0700

    feat(ListOrders): add tabs for time-based order filtering

--- app/Filament/Resources/OrderResource/Pages/ListOrders.php
+++ app/Filament/Resources/OrderResource/Pages/ListOrders.php
@@ -4,7 +4,9 @@ namespace App\Filament\Resources\OrderResource\Pages;
 
 use App\Filament\Resources\OrderResource;
 use Filament\Actions;
+use Filament\Forms\Components\Tabs\Tab;
 use Filament\Resources\Pages\ListRecords;
+use Illuminate\Database\Eloquent\Builder;
 
 class ListOrders extends ListRecords
 {
@@ -14,6 +16,37 @@ class ListOrders extends ListRecords
     {
         return [
             Actions\CreateAction::make(),
+        ];
+    }
+
+    public function getTabs(): array
+    {
+        return [
+            'All' => Tab::make('All'),
+            'Today' => Tab::make('Today')
+                ->modifyQueryUsing(fn (Builder $query) => $query->where('created_at',
+                    '>=', now()->subDay())),
+            'This week' => Tab::make('This week')
+                ->modifyQueryUsing(fn (Builder $query) => $query->where('created_at',
+                    '>=', now()->subWeek()
+                )),
+           'Last week' => Tab::make('Last week')
+                ->modifyQueryUsing(function (Builder $query) {
+                    return $query->where('created_at', '>=', now()->subWeek()->startOfDay());
+                }),
+            'This month' => Tab::make('This month')
+                ->modifyQueryUsing(fn (Builder $query) => $query->where('created_at',
+                    '>=', now()->subMonth())
+                ),
+            'This quarter' => Tab::make('This quarter')
+                ->modifyQueryUsing(fn (Builder $query) => $query->where('created_at',
+                    '>=', now()->subQuarter())
+                ),
+            'This year' => Tab::make('This year')
+                ->modifyQueryUsing(fn (Builder $query) => $query->where('created_at',
+                    '>=', now()->subYear())
+                ),
+
         ];
     }
 }
