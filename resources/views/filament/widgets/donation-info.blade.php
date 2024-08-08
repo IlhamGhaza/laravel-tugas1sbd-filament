effaeeb5593e35e8b43a60be8160e72b7ab68410
Ilham Ghazali <m.ilhamghazali@gmail.com>
2024-08-09 06:34:45 +0700

    feat(donation-info): Update donation link to Ko-Fi

--- resources/views/filament/widgets/donation-info.blade.php
+++ resources/views/filament/widgets/donation-info.blade.php
@@ -38,7 +38,7 @@
             <p class="mt-4">Support saya</p><br>
 
             <ul class="mt-4 space-y-2">
-                <li>🧡 <a href="https://patreon.com/your-patreon" class="link-orange">Sponsori saya di Patreon</a></li>
+                <li>🧡 <a href="https://ko-fi.com/ilhamghaza" class="link-orange">Donate</a></li>
                 <li>⭐ <a href="https://github.com/IlhamGhaza/laravel-tugas1sbd-filament" class="link-blue">Ikuti dan star repository kami</a></li>
                 <li>🐛 <a href="https://github.com/IlhamGhaza/laravel-tugas1sbd-filament/issues" class="link-green">Laporkan bug atau masalah</a></li>
                 <li>🗣️ <a href="https://github.com/IlhamGhaza/laravel-tugas1sbd-filament" class="link-purple">Bantu terjemahkan paket kami</a></li>
